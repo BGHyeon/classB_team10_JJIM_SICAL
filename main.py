fb61019b1ac5e9a89925f8d1ae640191cb70e495
g <>
2022-05-09 15:46:10 +0900

    개발해야할 기능들 틀 잡아 두었습니다.

--- main.py
+++ main.py
@@ -1,8 +1,71 @@
-# 샘플 Python 스크립트입니다.
+from flask import Flask, render_template,request,jsonify
+from pymongo import MongoClient
+from apscheduler.schedulers.background import BackgroundScheduler
+from bs4 import BeautifulSoup
+import uuid
+client = MongoClient("mongodb+srv://admin:admin@cluster0.16hc5.mongodb.net/Cluster0?retryWrites=true&w=majority")
+db = client.jjimsical
+app = Flask(__name__)
+sched = BackgroundScheduler(daemon=True)
+# login 관련 기능 (종연)
+@app.route('/login',methods=['GET'])
+def login():
+    return render_template('')
 
-# ⌃R을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
+@app.route('/login',methods=['POST'])
+def login_request():
+    print('hello')
+    return
 
-print('이거 확인 가능한가요?')
+# 회원가입 관련 기능 (승현)
+@app.route('/join',methods=['GET'])
+def join():
+    return render_template('')
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+@app.route('/join',methods=['POST'])
+def join_request():
+    print('hello')
+    return
+
+@app.route('/idcheck',methods=['POST'])
+def join_request():
+    print('hello')
+    return
+
+
+# 메인 페이지 관련 기능 개발(규현, 승재)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info',methods=['POST'])
+def get_musical_info():
+    return
+
+@app.route('/add/comment',methods=['POST'])
+def add_comment():
+    return
+
+@app.route('/add/favorite',methods=['POST'])
+def add_favorite():
+    return
+
+@app.route('/remove/comment',methods=['POST'])
+def remove_comment():
+    return
+
+@app.route('/remove/favorite',methods=['POST'])
+def remove_favorite():
+    return
+
+def refreshData():
+    return
+
+@sched.scheduled_job('cron',hour='0',minute='0',id='initdata')
+def crawlingInfo():
+    return
+
+sched.start()
+
+if __name__ == '__name__':
+    app.run('0.0.0.0',port=5000,debug=True)
